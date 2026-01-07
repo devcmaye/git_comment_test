71e7f64123d50bf49b7ae9d2bbd1b36bdd057d31
Cham Myae <cmaye.esc.dev@gmail.com>
2026-01-07 09:34:22 +0630

    Start Create Project

---
+++ lib/git_comment_test.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class GitCommentTest extends StatefulWidget {
+  const GitCommentTest({super.key});
+
+  @override
+  State<GitCommentTest> createState() => _GitCommentTestState();
+}
+
+class _GitCommentTestState extends State<GitCommentTest> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
